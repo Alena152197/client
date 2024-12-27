e4ca4780807fdc682d6ddec7cf6409d914cd5546
Furajkova Alena <Furajkova@mail.ru>
2024-12-27 17:19:38 +0300

    feb fn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@pinia/nuxt'],
-  pinia: {
-    storesDirs: ['./stores/**'],
-  },
+  modules: ['@pinia/nuxt']
 })
